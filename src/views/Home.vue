e2c9889a9f67264d2480e41c9677d88b61d6b689
pengshengguang <499964903@qq.com>
2021-11-22 13:10:55 +0800

    feat(git): 使用Commitizen规范提交代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -13,6 +14,9 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created() {
+    console.log('123')
   }
 }
 </script>
